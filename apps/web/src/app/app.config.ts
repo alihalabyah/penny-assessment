31086608f8406c1a038badfaddf2281dc7e61ee6
Ali Halabyah <alihalabyah@gmail.com>
2024-10-31 15:39:40 +0300

    chore: backend

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -2,9 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(),
+    provideStore(),
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
